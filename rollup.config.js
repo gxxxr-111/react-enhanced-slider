c3e93ab012c5b0c1310e49d536e4623f73c8cd69
Mirage_ <gaoxuran111@gmail.com>
2025-03-03 19:52:40 +0800

    fix: rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,46 @@
+import path from 'path';
+import $ from 'jquery';
 import typescript from '@rollup/plugin-typescript';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
-    input: 'src/index.ts',
+    input: 'src/index.ts',  // Input file
     output: [
         {
-            file: 'dist/index.js',
+            file: 'dist/index.cjs.js',  // Output as CommonJS format
             format: 'cjs',
+            sourcemap: true,  // Enable source map
         },
         {
-            file: 'dist/index.esm.js',
+            file: 'dist/index.esm.js',  // Output as ES module format
             format: 'esm',
+            sourcemap: true,  // Enable source map
+        },
+        {
+            file: 'dist/index.umd.js',  // Output as UMD format
+            format: 'umd',
+            name: 'Slider',  // Global variable name exposed in UMD format
+            sourcemap: true,
+            plugins: [terser()],  // Minify the UMD output
+            globals: {
+                jquery: '$',  // Map jQuery to the global variable '$'
+                react: 'React',        // Map React to the global variable 'React'
+                'react-dom': 'ReactDOM', // Map ReactDOM to the global variable 'ReactDOM'
+            },
         },
     ],
     plugins: [
-        typescript(),
-        resolve({ extensions: ['.js', '.ts', '.tsx'] }),
-        commonjs()
+        peerDepsExternal(),  // Automatically externalize peer dependencies
+        resolve(),  // Resolve modules in node_modules
+        commonjs(),  // Convert CommonJS modules to ES6
+        typescript({ tsconfig: './tsconfig.json' }),  // Compile TypeScript using tsconfig.json
     ],
-    external: ['react', 'react-dom'],
+    external: [
+        'react',  // Exclude React as a peer dependency
+        'react-dom', // Exclude ReactDOM as a peer dependency
+        'jquery',  // Exclude jQuery as a peer dependency
+    ],
 };
